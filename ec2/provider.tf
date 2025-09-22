0758f0f7a558ad528d4271e8b4372e9832e848fb
Parvez1825 <parvez.mohammed@yash.com>
2025-09-22 12:16:18 +0530

    Update provider.tf

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -8,7 +8,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket  = "devops-terraform-123"
+    bucket  = "devops-terraform-1234"
     key     = "ec2/ec2.tfstate"
     region  = "us-east-1"
     encrypt = true
